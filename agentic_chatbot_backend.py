72a84ddea3e7dcd1bde1aed65ff67867b2138a19
thilanhasithaAI <thilanhasithayasanjith26@gmail.com>
2026-07-10 17:03:06 +0530

    added some part of codes here

---
+++ agentic_chatbot_backend.py
@@ -0,0 +1,38 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_ollama import ChatOllama
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOllama(model='llama3.2')
+
+
+## define the state
+class ChatState(TypedDict):
+    messages:Annotated[list[BaseMessage],add_messages]
+
+#define the chat_node
+def chat_node(state: ChatState):
+    #take user query from state
+    messages = state['messages']
+    # send to llm
+    response = llm.invoke(messages)
+    # response store state
+    return {'messages': [response]}
+
+checkpoint = MemorySaver()
+
+graph = StateGraph(ChatState)
+
+## add nodes
+graph.add_node('chat_node', chat_node)
+
+#add edges
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpoint)
--- app.py
+++ app.py
@@ -2,22 +2,42 @@ import streamlit as st
 from agentic_chatbot_backend import chatbot
 from langchain_core.messages import HumanMessage, BaseMessage
 
+
+st.title("Agentic AI with Langgraph")
+
 CONFIG = {
     "configurable": {
         "thread_id": "thread_1",
     }
 }
 
-st.title("Agentic AI with Langgraph")
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+#loading the conversational history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+
+
 
 user_input = st.chat_input("Enter your message:", key="user_input")
 
 if user_input:
+    #first add the message into the message history
+    st.session_state['message_history'].append({'role':'user', 'content':user_input})
+
+    
     with st.chat_message("user"):
         st.markdown(user_input)
 
     response = chatbot.invoke({'messages':[HumanMessage(content=user_input)]}, config=CONFIG)
 
     ai_message = response['messages'][-1].content
+
+    #first add the message into the message history
+    st.session_state['message_history'].append({'role':'assistant', 'content':ai_message})
+
     with st.chat_message("assistant"):
      st.text(ai_message)
